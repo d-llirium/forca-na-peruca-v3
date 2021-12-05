319d173122e4887f9b9ff4a443bd428860cba9fe
d-llirium <plimacarlos@gmail.com>
2021-12-05 20:22:44 -0300

    insere as tabs na interface

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,9 +4,24 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: '', // -- para referenciar a própria página mãe que é a home
     component: HomePage,
-  }
+    children: 
+    [ // -- filhas que entram na TAB
+      {
+        path: 'como-tab',
+        loadChildren: () => import('../como-tab/como-tab.module').then( m => m.ComoTabPageModule)
+      },
+      {
+        path: 'sobre',
+        loadChildren: () => import('../sobre/sobre.module').then( m => m.SobrePageModule)
+      },
+      {
+        path: 'como-projeto',
+        loadChildren: () => import('../como-projeto/como-projeto.module').then( m => m.ComoProjetoPageModule)
+      },
+    ]
+  },
 ];
 
 @NgModule({
