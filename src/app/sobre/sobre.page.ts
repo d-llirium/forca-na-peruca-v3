678a01acd6ee27641bebcc332ccc744022a220e3
d-llirium <plimacarlos@gmail.com>
2021-12-07 19:40:04 -0300

    usando diretivas

--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -1,14 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IRecurso
+{
+  nome: string;
+  icone: string;
+}
+interface IReferencia
+{
+  icone: string;
+  referencia: string;
+}
+
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
 })
-export class SobrePage implements OnInit {
+export class SobrePage implements OnInit 
+{
+  // ATRIBUTOS
+  recursos: IRecurso[] = 
+  [
+    {
+      nome: "macbook", 
+      icone: "./assets/icon/apple-icon.png",
+    },
+    {
+      nome: "visual studio code",
+      icone: "./assets/icon/visual-code-icon.png"
+    },
+    {
+      nome: "chrome",
+      icone: "./assets/icon/chrome-icon.png"
+    }
+      
+  ];
+  referencias: IReferencia[] =
+  [
+    {
+      icone: "./assets/icon/apple-icon.png",
+      referencia: "https://support.apple.com/pt-br/HT210222",
+    },
+    {
+      icone: "./assets/icon/visual-code-icon.png",
+      referencia: "https://code.visualstudio.com/download",
+    },
+    {
+      icone: "./assets/icon/chrome-icon.png",
+      referencia: "https://support.google.com/chrome/answer/95346?hl=pt-BR&co=GENIE.Platform%3DDesktop",
+    }
+  ]
 
+
+  //construtor
   constructor() { }
-
+  //MÉTODOS
   ngOnInit() {
   }
 
